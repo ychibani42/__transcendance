bf50d1c9f0a845238b529aefd71c8209bb94de09
tliot <liot@student.42.fr>
2023-07-19 17:32:39 +0000

    Front + back

---
+++ srcs/frontend/nextjs/src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <div className="Navbar ml-5">
+	  <h1 className="text-bold">Menu</h1>
+      <div className="NavElements ml-10">
+	  <li><a href="http://localhost:3000">Pouecccctte 1</a></li>
+	  <li><a href="http://localhost:3000">Pouette 2</a></li>
+	  <li><a href="http://localhost:3000">Pouette 3</a></li>
+      </div>
+    </div>
+  );
+}
+ 
