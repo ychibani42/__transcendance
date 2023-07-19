a6eee592d5ec5dfcdc0a677ff38f5302ae986091
mychibani <m.yassinechibani@gmail.com>
2023-07-19 19:16:11 +0200

    fix: dockerfile + backend + postgres + add of heathcheck of the db

---
+++ srcs/back-end/server_launch.sh
@@ -0,0 +1,20 @@
+npm install
+
+# Install nodemon and ts-node as dev dependencies
+npm install --save-dev nodemon ts-node
+
+# This line updates all the dependencies defined in the package.json file 
+npm update
+
+# Generate database 
+npx prisma generate
+
+# Update database migration 
+npx prisma migrate dev
+
+# Push to database last changes
+npx prisma db push
+
+# Give permissions to use xdg open for prisma studio
+
+npm run start:dev
