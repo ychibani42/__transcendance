1c95c62a312d7b9fc07e932d8d2aba0d3f08793d
mychibani <m.yassinechibani@gmail.com>
2023-08-02 15:45:57 +0200

    fix(cors policy fix)

--- srcs/back-end/src/main.ts
+++ srcs/back-end/src/main.ts
@@ -2,7 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	const options = {
+		origin: true,
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		preflightContinue: false,
+		credentials: true,
+		allowedHeaders: 'Content-Type, Accept',
+	};
+	app.enableCors(options);
+	await app.listen(3000);
 }
 bootstrap();
